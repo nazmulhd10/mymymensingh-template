ae786671012dffda59ef088c73163cd055f9fd74
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-01 02:14:44 +0600

    hero section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
         }
     });
 
-    // $(".dropdown-toggle").dropdown();
 
 
 
@@ -31,77 +30,27 @@ $(document).ready(function () {
         }
     });
     // feature carousel
-    $('.feature-corosel').owlCarousel({
+    $('.image-carousel').owlCarousel({
         loop: true,
         margin: 0,
         autoplay: true,
         responsiveClass: true,
+        nav: true,
         responsive: {
             0: {
                 items: 1,
             },
             600: {
-                items: 2,
+                items: 1,
             },
             1000: {
-                items: 3,
+                items: 1,
             }
         }
     });
     // screenshort carousel
-    $('.screenshort-crosel').owlCarousel({
-        loop: true,
-        margin: 0,
-        autoplay: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 4,
-            }
-        }
-    });
-    // testimonials carousel
-    $('.testimonials-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        autoplay: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
-    // team carousel
-    $('.team-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        autoplay: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
+
+
     // page scroling sectin
     $.scrollIt({
         topOffset: -50
